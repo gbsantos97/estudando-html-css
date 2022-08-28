40b95f16a641aa4649434e405bdd976c89c3e279
Gabriel <gabrielpires599@gmail.com>
2022-08-28 20:02:30 -0300

    atualizando estudos

---
+++ 06 - Finalmente JS/começandojs.js
@@ -0,0 +1,26 @@
+console.log('Bem vindos ao starter')
+                                         /*comentario em linha*/
+console.log(1 + 1)                       /*aqui o console entende 2*/
+console.log(`aqui crases q 
+permite quebra de linha`)                /*em vez de '' ou "" eu usei `` crases sequencia logica `<maior "=''="maior>` alem de permitir multplas linhas ainda permite expreções de linguagem ex ${1+1}*/ 
+console.log(false)                       /*falso 'booleano'*/
+console.log(true)                        /*verdadeiro 'booleano' booleano aquele que só tem 2 valores verdadeiro ou falso */
+console.log(undefined)                   /*algo indefinido  (é algo que não vai existir)*/
+console.log(null)                          /* (esse existe mas não há nada nele ) */
+/*
+Objeto  ele contem
+*Propriedade     / Atributos
+*Funcionalidades / Métodos
+{ propriedade: "valor"}
+
+*/
+console.log({
+    name: "gabriel",
+    idade:"25",
+    andar: function() {
+        console.log('andar') 
+}
+    
+})
+
+
