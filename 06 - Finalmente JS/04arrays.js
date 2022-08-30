6708fed40a0ad70a06594475c9ce5dbb77d29961
Gabriel <gabrielpires599@gmail.com>
2022-08-30 17:10:48 -0300

    atualizando

---
+++ 06 - Finalmente JS/04arrays.js
@@ -0,0 +1,8 @@
+// Array
+const animals = [
+    'Lion',
+    'Monkey',
+    'Cat'
+]
+//acessar valores dentro do array   é necessario ex [2] ou lenght para saber o total de elementos
+console.log(animals[2])
---
+++ 06 - Finalmente JS/05exercicio.js
@@ -0,0 +1,39 @@
+// let ;
+// console.log(typeof weight)
+// let name = "Gabriel" // (String "ooooo")
+// let age = 25         // (integer numeros inteiros)
+// let weight = 67.5     // (Float numeros quebrados)
+// isSubscrible = true  // (Boolean true or false)
+
+let student = {
+    
+     name : "Gabriel" ,
+     age : 25  ,          
+     weight: 67.5,
+     isSubscrible : true,
+    };    
+// console.log (`o ${student.name} tem ${student.age} anos de idade, e pesa ${student.weight}KG.`)
+ 
+let students = [] 
+
+
+ students = [
+    student
+ ]
+    
+
+//  console.log (students)   
+//  console.log (students[0])
+
+ const jhon = {
+    name : "Jhon",
+    age : 20,
+    weight : 80.5,
+    isSubscrible : true
+
+ }
+students = [
+    student,
+    jhon
+]
+console.log(students)
