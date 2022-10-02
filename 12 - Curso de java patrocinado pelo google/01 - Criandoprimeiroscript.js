ab94c9c78108167437fa4fa61148885970b33ae8
Gabriel <gabrielpires599@gmail.com>
2022-10-02 05:09:33 -0300

    atualizando

--- 12 - Curso de java patrocinado pelo google/01 - Criandoprimeiroscript.js
+++ 12 - Curso de java patrocinado pelo google/01 - Criandoprimeiroscript.js
@@ -19,14 +19,15 @@
  s.length                    - quantos caracteres a string tem
  s.toUpperCase()             - tudo para'MAIUSCULAS'
  s.toLowerCase()             - tudo para 'minusculas'
- toFixed()                   - para o numero de casas decimais
+ toFixed()                   - para o numero de casas decimais ex 5.5 
  replace()                   - usei para trocar os pontos por virgula
 */
-var nome = window.prompt('Qual é seu nome?')
-document.write(`Ola, <strong> ${nome}</strong> seu nome tem ${nome.length} letras.</br>`)
-document.write(`Seu nome todo em maiusculas é ${nome.toUpperCase()}</br>`)
-document.write(`Seu nome todo em minusculas é ${nome.toLowerCase()}</br>`)
-/* var n1 = 1545.5
+// var nome = window.prompt('Qual é seu nome?')
+// document.write(`Ola, <strong> ${nome}</strong> seu nome tem ${nome.length} letras.</br>`)
+// document.write(`Seu nome todo em maiusculas é ${nome.toUpperCase()}</br>`)
+// document.write(`Seu nome todo em minusculas é ${nome.toLowerCase()}</br>`)
+/* BRINCANDO NO TERMINAL
+var n1 = 1545.5
                  Resposta undefined
  n1
                  Resposta 1545.5
@@ -36,4 +37,71 @@ document.write(`Seu nome todo em minusculas é ${nome.toLowerCase()}</br>`)
                   Resposta'1545,50'
  n1.toLocaleString('pt-BR', {style:'currency' , currency: 'BRL'})
                   Resposta'R$ 1.545,50'
+*/
+
+
+              // AULA 7 operadores aritimedicos
+/*
++ adição
+- subtração
+* multiplicação
+/ divisão
+% divisão inteira (resto da divisao ou oque sobra)
+** ao quadrado (Potencia)
+
+       Ordem de Precedencia dos operadores aritimedicos
+()
+**
+* / %
++ -
+exemplo na imagem Precedenciaaritimedica.png
+ */
+
+
+
+          //AULA 8 Operadores relacionais
+/* os resultados são sempre valores booleanos true ou false
+>  (- maior)
+<  (- menor)
+>= (- maior ou igual)
+<= (- menor ou igual)
+== (- igual)
+!= (- não igual)
+
+ 
+        OPERADORES DE IDENTIDADE
+===(- extritamente igual)
+    
+    EX:
+    5 == 5 true
+    5 == '5'true
+    5 === '5' falso
+     
+        OPERADORES LOGICOS ja na ordem de precedencia
+1 - !  (- Negação)
+2 - && (- Conjunção só se da por satisfeito se os dois valores for verdadeiro)
+3 - || (- Disjunção satisfaz com apenas um dos valores for verdadeiro)
+ 
+EM PORTUGUES
+idade >= && idade <= 17            //a idade esta entre 15 e 17 ?
+estado == 'RJ' || estado == 'SP'   //o estado é RJ ou SP?
+salario > 1500 && sexo != 'M'      //o salario é acima de 1500 e não é homem?
+
+ Ordem de Precedencia
+1 - Aritimedicos 
+2 - Relacionais 
+3 - Logicos
+4 - ternario
+
+OPERADOR TERNARIO
+? :
+EX
+var media = 5.5
+media > 7 ? 'Aprovado':'Reprovado' (media é maior que 7 ? se sim Aprovado se nao reprovado)
+(media>7?      'true' : 'false'   )               
+*/
+
+             //AULA 9 INTRO DOM
+/*
+
 */
